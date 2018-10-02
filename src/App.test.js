c29c11f94fd627df67a340e32f6e6ba6e3dc20f8
Oliver Kenyon <oliverkenyon@hotmail.com>
2018-10-02 14:51:32 +0100

    Get test into passing state

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import fetchMock from 'fetch-mock';
 
-it('blah', () => {
-  fetchMock.mock('http://example.com', 200);
+beforeEach(() => {
+  const fetchMock = require('fetch-mock');
+  fetchMock.mock('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=2b132887161e4634a9319d61616b1de8', '{}');
+});
 
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+ 
 });
